b4b5b2235bb2e8a0793d269b5c4c52077c0702fb
NickCampbell97 <njc1749@gmail.com>
2024-03-29 14:48:42 -0500

    modified:   frontend/src/components/design_pane/SlideOptionsButton.jsx 	modified:   frontend/src/components/design_pane/SlideWindow.jsx 	modified:   frontend/src/components/design_pane/design_styles/styles.css

--- frontend/src/components/design_pane/SlideOptionsButton.jsx
+++ frontend/src/components/design_pane/SlideOptionsButton.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import { IconButton } from '@material-ui/core';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import React, { useState } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-export default function SlideOptionButton() {
+const SlideOptionButton = ({ onItemClick }) => {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const menuItems=[
+        { label: 'Edit', action: 'action1' },
+        { label: 'Duplicate', action: 'action2' },
+        { label: 'Delete', action: 'action3' }
+    ];
+
+    const onClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleMenuItemClick = (action) => {
+        onItemClick(action);
+        handleClose();
+    };
+
     return (
-        <IconButton style={{ minHeight: '20px', minWidth: '20px', position: 'absolute', top: '1px', right: '3px', padding: '2px' }}>
-            <MoreHorizIcon fontSize='medium' />
-        </IconButton>
+        <div>
+            <IconButton 
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={onClick}
+                style={{ minHeight: '20px', minWidth: '20px', position: 'absolute', top: '1px', right: '3px', padding: '3px' }} 
+            >
+                <MoreHorizIcon fontSize='medium' />
+            </IconButton>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                {menuItems.map((menuItem, index) => (
+                    <MenuItem key={index} onClick={() => handleMenuItemClick(menuItem.action)}>
+                        {menuItem.label}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
     );
 };
+
+export default SlideOptionButton;
--- frontend/src/components/design_pane/SlideWindow.jsx
+++ frontend/src/components/design_pane/SlideWindow.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { Stack } from '@mui/material';
+import { IconButton } from '@material-ui/core';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import SlideOptionButton from './SlideOptionsButton';
 import './design_styles/styles.css';
 
@@ -21,6 +23,14 @@ const SlideWindow = ({ color, slideData }) => {
 
     const fileList = getFiles(initialFileList);
 
+    const handleHeaderItemClick = (action) => {
+        console.log(`Clicked: ${action}`);
+    };
+
+    const handleBodyItemClick = (action) => {
+        console.log(`Clicked: ${action}`);
+    };
+
 
     return (
         <Box
@@ -35,26 +45,28 @@ const SlideWindow = ({ color, slideData }) => {
             }}
         >
             {slideData && (
-                <div className='slide-container'>
+                <div className='slide-container' style={{ alignItems: 'center' }}>
                     <div style={{ position: 'relative' }}>
-                        <Typography variant='h3' className='bordered-container' style={{ marginTop: '10px' }}>
-                            <SlideOptionButton />
+                        <Typography variant='h4' className='bordered-container' style={{ marginTop: '5px' }}>
+                            <SlideOptionButton onItemClick={handleHeaderItemClick}/>
                             {slideData.header}
                         </Typography>
                     </div>
                     {bodyList.map((item, index) => (
                         <div style={{ position: 'relative' }}>
                             <Typography key={index} variant='body1' className='body-container' style={{ marginTop: '10px' }}>
-                                <SlideOptionButton />
+                                <SlideOptionButton onItemClick={handleBodyItemClick}/>
                                 {item}
                             </Typography>
                         </div>
                     ))}
-                    <Stack spacing={4} direction="row" style={{ alignItems: 'center', marginTop: '8px', minWidth: '250px' }}>
+                    <Stack spacing={4} direction="row" style={{ alignContent: 'center', marginTop: '8px', minWidth: '250px' }}>
                     {fileList.map((item, index) => (
                         <div style={{ position: 'relative' }}>
                             <Typography key={index} variant='body1' className='file-container' style={{ marginTop: '10px' }}>
-                                <SlideOptionButton />
+                                <IconButton style={{ minHeight: '20px', minWidth: '20px', position: 'absolute', top: '4px', right: '4px', padding: '4px' }}>
+                                    <MoreHorizIcon fontSize='medium' />
+                                </IconButton>
                                 {item}
                             </Typography>
                         </div>
